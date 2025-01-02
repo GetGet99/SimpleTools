3994044dbb1a8f525eecff8d223ac3022c4ddbda
getdn <getdna99@gmail.com>
2025-01-02 02:20:21 -0800

    move dark mode scripts way up in the website

--- setup.js
+++ setup.js
@@ -1,18 +1,5 @@
 // prevent variables from being exported
 (function () {
-    if (window.matchMedia) {
-        const preferesDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
-        if (preferesDarkScheme.matches) {
-            document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", "dark")
-        }
-        preferesDarkScheme.addEventListener('change', function() {
-            if (preferesDarkScheme.matches) {
-                document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", "dark")
-            } else {
-                document.getElementsByTagName("html")[0].removeAttribute("data-bs-theme")
-            }
-        })
-    }
     const backIcon = document.createElement("i")
     backIcon.className = "bi bi-arrow-left"
     const backbtn = document.createElement("a");
